99ccf5e43d9c6e9fc7a0e45a52ec5b938b3c40de
narita <kn.narita@gmail.com>
2025-09-27 23:00:55 +0900

    initial commit

---
+++ ConsoleApp1/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MyClass<T>
+{
+    public T prop { get; private set; }
+
+    public MyClass(T value)
+    {
+        prop = value;
+    }
+    public void MyMethod()
+    {
+        Console.WriteLine("property : " + prop);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 修正: ジェネリック制約を削除し、正しいインスタンス化方法に修正
+MyClass<string> test1 = new MyClass<string>("test");
+test1.MyMethod();
+MyClass<int> test2 = new MyClass<int>(10);
+test2.MyMethod();
+
+string str = "aaaa";
+// 修正: ジェネリック型引数には型名を直接指定する必要があります
+MyClass<string> test3 = new MyClass<string>(str);
+Console.WriteLine("type = " + str.GetType());
+test3.MyMethod();
